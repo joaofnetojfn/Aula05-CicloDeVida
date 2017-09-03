6a5ff3da171595e5575dffd4a11315566a99f4f1
joaofnetojfn <joaofnetojfn@gmail.com>
2017-09-03 19:40:29 -0300

    Initial commit

---
+++ app/src/main/java/com/example/joaoferreira/aula05_ciclodevida/MainActivity.java
@@ -0,0 +1,8 @@
+package com.example.joaoferreira.aula05_ciclodevida;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends DebugActivity {
+
+}
